88a78c4d18d4e952d5240126eefc865b5e435966
s3cpat <s3cpat@gmail.com>
2018-03-30 16:50:03 -0400

    add heart rate program for Wen to do

---
+++ hr.py
@@ -0,0 +1,22 @@
+# Determine Target Heart Rate Zone
+# Assigned to: Wen
+
+# Comments describe what needs to be done, obviously ;P
+
+# prompt user for age
+
+# calculate max heart rate by doing 220 - age
+
+# print max hr
+
+# prompt user for resting heart rate, or allow user to enter a ? for 65 as a guess
+
+# calculate Heart Rate Reserve (HRR); HRR=MaxHR-RestHR
+
+# calculate lower bound of training zone (70% of HRR)
+
+# calculate upper bound of training zone (85% of HRR)
+
+# print lower and upper bound in the format [Lower, Upper]
+
+print("todo")
