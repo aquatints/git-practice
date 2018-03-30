b2bf908650ae3993d807a12a8e3c643df8ab6fde
Wendy Davis <8wendydavis@gmail.com>
2018-03-30 18:35:41 -0400

    Wrote the program. I did it.

--- hr.py
+++ hr.py
@@ -5,18 +5,47 @@
 
 # prompt user for age
 
-# calculate max heart rate by doing 220 - age
+age = input ("Input age: ")
+while age <= 0:
+	print "that is not a valid age. Please try again"
+	age = input ("Input age: ")
 
-# print max hr
+	#something for upper bounds of age? if older than oldest recorded person say "Wow! You sould call the Guiness book of world records"
+
+# calculate max heart rate by doing 206.9 - (0.67 * age)
+
+maxHR = (206.9 - (0.67 * age))
+
+# print max hr 
+
+print ("Maximum heart rate: " + str(maxHR))
+
+'''
 
 # prompt user for resting heart rate, or allow user to enter a ? for 65 as a guess
 
+restHR = raw_input ("Input resting heart rate (if you do not know, enter '?'): ")
+if restHR == "?":
+	restHR = 65
+
+restHR = int(restHR)
+
 # calculate Heart Rate Reserve (HRR); HRR=MaxHR-RestHR
 
-# calculate lower bound of training zone (70% of HRR)
+HRR = (maxHR - restHR)
 
-# calculate upper bound of training zone (85% of HRR)
+print ("HRR " + str(HRR))
+
+'''
+
+# calculate lower bound of SPORADIC training zone (74% of maxHR)
+
+LB = (.74 * maxHR)
+
+# calculate upper bound of SPROADIC training zone (84% of maxHR)
+
+UB = (.84 * maxHR)
 
 # print lower and upper bound in the format [Lower, Upper]
 
-print("todo")
+print ("[" + str(LB) + ", " + str (UB) + "]")
